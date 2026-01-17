31e38aabddfc113421014d741cedab9c000e4ef1
Taush Sampley <taushsampley@gmail.com>
2026-01-17 15:48:36 -0600

    Update ESLint configuration to include strict and stylistic rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 import globals from 'globals'
 
+// TODO: try https://typescript-eslint.io/getting-started/typed-linting/
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
   {
     languageOptions: {
       globals: {
