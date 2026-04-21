d22a47376b07f88ecc1fbd99ecb5df2dffbecab6
Taush Sampley <taushsampley@gmail.com>
2026-04-21 17:43:12 -0500

    Add all-catching error middleware

--- src/social/social-service.ts
+++ src/social/social-service.ts
@@ -1,6 +1,9 @@
 
 import { socialRouting } from '../social/social-routing.js'
 import { Service } from '../common/service.js'
+import { SocialController } from './social-controller.js'
+import { SocialDatabase } from './data/social-db.js'
+import type { Request, Response } from 'express'
 
 /**
  * The Social Service 
@@ -17,7 +20,14 @@ export class SocialService extends Service {
 
   override async start(port:number) {
     // Setup API Routing
-    socialRouting(this.app)
+    socialRouting(this.app, this.controller)
+
+    this.app.use((error:unknown,req:Request,res:Response,next:()=>void)=> {
+      // Report error for debugging and log aggregation, but do not disclose details to users for security
+      console.error('Uncaught Error: ', error)
+      res.status(500).send{{message:'Internal Server Error - Cannot Disclose Details'}}
+      next()
+    })
 
     // Start Service App
     this.app.listen(port, ()=> {
