45149c20bd07aa38b59491bcda1ef95379ec8d2c
Taush Sampley <taushsampley@gmail.com>
2026-04-21 17:40:31 -0500

    Add social database segregation

--- src/social/data/social-db.ts
+++ src/social/data/social-db.ts
@@ -1,9 +1,10 @@
 import { DatabaseSync, type SQLOutputValue } from "node:sqlite"
-
 import { Comment } from "../entities/comment.js"
 import { randomUUID, type UUID } from "node:crypto"
 import { PgFacade } from "../../data/pg-facade.js"
 import type { Pool } from "pg"
+import type { Feed } from "../entities/feed.js"
+import type { Reaction } from "../entities/reaction.js"
 
 const TABLE_FEEDS = `
 CREATE TABLE feeds (
@@ -18,49 +19,73 @@ CREATE TABLE comments (
   content TEXT NOT NULL
 );`
 
-export class SocialDatabase {
+export interface FeedDatabase {
+  createFeed(title:string): Promise<Feed>
+  readFeed(id:UUID): Promise<Feed>
+  updateFeed(id:UUID,title:string): Promise<Feed>
+  deleteFeed(id:UUID): Promise<void>
+}
+export interface CommentDatabase {
+  createComment(content:string,feedId:UUID,parentId?:UUID): Promise<Comment>
+  readComment(id:UUID): Promise<Comment>
+  readComments(feedId:UUID): Promise<Comment[]>
+  updateComment(id:UUID,content:string): Promise<Comment>
+  deleteComment(id:UUID): Promise<void>
+}
+export interface ReactionDatabase {
+  upsertCommentReaction(commentId:UUID,reaction:Reaction): Promise<Reaction[]>
+  readCommentReactions(commentId:UUID): Promise<Reaction[]>
+}
+export class SocialDatabase { // TODO: implement feed, comment, reaction databases; only use narrow interfaces in controller so we can separate them behind the scenes
   private db: DatabaseSync
   constructor(path:string=':memory:') {
     this.db = new DatabaseSync(path)
   }
 
-  async addComment(feedId:string,parentId:string,content:string): Promise<Comment> {
-
+  async createComment(feedId:UUID,content:string,parentId?:UUID,): Promise<Comment> {
+    
     return new Promise<Comment>((resolve,reject)=>{
       try {
         const uuid = randomUUID() // generate a UUID for the comment
         const statement = this.db.prepare(`
-            INSERT INTO comments (feed_id,parent_id,content)
-            VALUES ($feedId,$parentId,$content)
-            `)
-        statement.run({
-          feedId: feedId,
-          parentId: parentId,
-          content: content,
-          uuid: uuid
-        }) 
-        resolve(new Comment(content,uuid))
+          INSERT INTO comments (feed_id,parent_id,content)
+          VALUES ($feedId,$parentId,$content)
+          `)
+          statement.run({
+            feedId: feedId,
+            parentId: parentId,
+            content: content,
+            uuid: uuid
+          }) 
+          resolve(new Comment(content,uuid))
+        }
+        catch (err) {
+          reject(err)
+        }
       }
-      catch (err) {
-        reject(err)
-      }
-    }
-  )
+    )
   }
-
+  
   async getComments(feedId:string): Promise<Comment[]> {
     const statement = this.db.prepare('SELECT (comment_id,content) FROM comments WHERE feed_id=$feedId LIMIT 10')
     const result = statement.all({
       feedId:feedId
     })
-
+    
     const commentList = result.map((value:Record<string,SQLOutputValue>)=>{
       const comment = value['comment']?.valueOf() as string || '<No Comment>'
       const uuid = value['uuid']?.valueOf() as UUID || '<No UUID>'
       return new Comment(comment, uuid)
     })
-
+    
     return commentList
+  }
+  setComment(id: string, content: string) {
+    // TODO: attempt to overwrite comment
+  
+  }
+  async deleteComment(id: string) {
+
   }
 }
 
