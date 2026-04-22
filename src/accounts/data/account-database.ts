a9085b049c25d58480478136df2b23f13a69aa7d
Taush Sampley <taushsampley@gmail.com>
2026-04-22 08:07:53 -0500

    refactor account database

--- src/accounts/data/account-database.ts
+++ src/accounts/data/account-database.ts
@@ -1,9 +1,13 @@
 
 import pg from "pg"
-import { PgFacade } from "../../data/pg-facade.js"
+import { PgFacade } from "@/data/pg-facade.js"
+import type { UUID } from "crypto"
+import { Account, DefaultImage } from "../entities/account.js"
 
 /**
- * 
+ * TODO: consider renaming to UserDatabase since it is meant to handle both
+ * public and private facets of user information. Consider defining separate database classes for public and private information to better ensure sanitation
+ * of private information and to better separate concerns.
  */
 export class AccountDatabase {
 
@@ -15,19 +19,26 @@ export class AccountDatabase {
     this.pool = pool
   }
   
-  createUser(name:string,password:string) {
+  async createAccount(name:string,password:string): Promise<Account> {
+    // TODO: create user in database and return created user
 
+    return new Account(name, new DefaultImage('blue')) // TODO: default image color should be randomly generated
   }
 
-  getUser(name?:string,email?:string) {
-
+  async readAccount(userId:UUID): Promise<Account> {
+    // TODO: read user from database and return user
+    return new Account('testAccount', new DefaultImage('blue')) // TODO: default image color should be randomly generated
   }
 
-  updateUser(id:string,name?:string,email?:string) {
+  async updateAccount(id:UUID,name?:string): Promise<Account> {
+    // TODO: update user in database and return updated user
 
+    return new Account(name ?? 'testAccount', new DefaultImage('blue')) // TODO: default image color should be randomly generated
   }
 
-  removeUser(id:string) {
+  async removeAccount(id:string) {
+    // TODO: remove user from database
+
 
   }
 }
