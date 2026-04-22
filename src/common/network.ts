f9a205761645fcaba2d2b2cc1129a9e031430e49
Taush Sampley <taushsampley@gmail.com>
2026-04-22 08:06:06 -0500

    Define standard service error handler

---
+++ src/common/network.ts
@@ -0,0 +1,15 @@
+
+import type { Request, Response } from "express"
+
+/**
+ * A middleware function to handle errors in the application. 
+ * @param error THe error caught
+ * @param req The request which is the origin of the error
+ * @param res The response object to send 
+ * @param next The chain callback to pass control to the next callback
+ */
+export function ErrorHandler(error:unknown,req:Request,res:Response,next:()=>void) {
+  // Report error for debugging and log aggregation, but do not disclose details to users for security
+  console.error('Uncaught Error: ', error)
+  res.status(500).send({message:'Internal Server Error - Cannot Disclose Details'})
+}
