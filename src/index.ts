2b7d7c60af31a6e4b51f5a0458b4302c1ab7d26c
Taush Sampley <taushsampley@gmail.com>
2026-04-22 08:19:03 -0500

    Pull service configuration from environment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 
 import startAccountService from './accounts/account-service.js'
+import { authSqlPool } from './accounts/data/account-database.js'
 import startSocialService from './social/social-service.js'
 
+console.log('==Welcome to the Pointyware API==')
+// Collect Arguments for Port Configuration, Passwords, etc.
+// TODO: Use a library like `commander` to handle CLI arguments and configuration
+const authPort = process.env.AUTH_PORT ? parseInt(process.env.AUTH_PORT) : 3001
+const authHost = process.env.AUTH_HOST || 'localhost'
+const socialPort = process.env.SOCIAL_PORT ? parseInt(process.env.SOCIAL_PORT) : 3002
+const socialHost = process.env.SOCIAL_HOST || 'localhost'
+
+// ==== Setup Dependencies
+const pool = await authSqlPool()
+
+// ==== Start Services
 // Accounts Service (authorization) must be running for anything to work
-startAccountService(3001)
+const accountService = await startAccountService(authPort, pool)
 // Start resource services
-startSocialService(3002)
+const socialService = await startSocialService(socialPort)
+
+console.log('Accounts Service: http://' + authHost + ':' + authPort)
+console.log('Social Service: http://' + socialHost + ':' + socialPort)
