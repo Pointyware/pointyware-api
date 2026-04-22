7ae07552161e2e1cd32ec2357673ac98958e24d4
Taush Sampley <taushsampley@gmail.com>
2026-04-22 08:08:40 -0500

    Add account adapter inputs

--- src/accounts/adapters/account.ts
+++ src/accounts/adapters/account.ts
@@ -1,7 +1,7 @@
 
 import { type Request } from "express"
 import type { AccountAuthDto, UserIdDto } from "../../data/dtos.js"
-import type { CreateAccountCommand, GetAccountCommand, LoginCommand } from "../domain/accounts.js"
+import type { CreateAccountCommand, EditUser, GetAccountCommand, LoginCommand, LogoutCommand } from "../domain/accounts.js"
 
 
 export function CreateAccountCommandMapper(
@@ -16,9 +16,26 @@ export function GetAccountCommandMapper(
     accountId: req.params.userId
   }
 }
+export function UpdateAccountCommandMapper(
+  req: Request<UserIdDto, any, AccountAuthDto, any>
+): EditUser {
+  return {
+    userId: req.params.userId,
+    username: req.body.username,
+    email: req.body.email
+  }
+}
 
 export function LoginCommandMapper(
   req: Request<any, any, AccountAuthDto, any>
 ): LoginCommand {
   return req.body
+}
+
+export function LogoutCommandMapper(
+  req: Request
+): LogoutCommand {
+  return {
+    userId: req.user.id
+  }
 }
--- src/accounts/domain/accounts.ts
+++ src/accounts/domain/accounts.ts
@@ -1,4 +1,6 @@
 import type { UUID } from "crypto";
+import { Account } from "../entities/account.js";
+import type { AccountDatabase } from "../data/account-database.js";
 
 export interface UserQuery {
   userId:UUID
@@ -12,7 +14,7 @@ export interface UserHandle {
   userId:UserId
 }
 export interface NewUser {
-  name:string,
+  username:string
   email:string
 }
 export type EditUser = NewUser & UserHandle
@@ -24,4 +26,45 @@ export interface CreateAccountCommand { //FIXME: reconcile with NewUser
 export interface LoginCommand {
   username:string
   password:string
+}
+export interface LogoutCommand {
+  userId:UUID
+}
+
+export function CreateAccount(database:AccountDatabase): (command:CreateAccountCommand)=>Promise<Account> {
+  return async function(command:CreateAccountCommand): Promise<Account> {
+    return database.createAccount(command.username, command.password)
+  }
+}
+
+export function GetAccount(database:AccountDatabase): (command:GetAccountCommand)=>Promise<Account> {
+  return async function(command:GetAccountCommand): Promise<Account> {
+    return database.readAccount(command.accountId)
+  }
+}
+
+export function UpdateAccount(database:AccountDatabase): (command:EditUser) => Promise<Account> {
+  return async function(command:EditUser): Promise<Account> {
+    return database.updateAccount(command.userId, command.name)
+  }
+}
+
+export function DeleteAccount(database:AccountDatabase): (command:UserHandle) => Promise<void> {
+  return async function(command:UserHandle): Promise<void> {
+    await database.removeAccount(command.userId)
+  }
+}
+
+export function Login(database:AccountDatabase): (command:LoginCommand)=>Promise<Account> {
+  return async function(command:LoginCommand): Promise<Account> {
+    // TODO: Implement login logic
+    return database.readAccount('') // TODO: read account by username, not id
+  }
+}
+
+export function Logout(database:AccountDatabase): (command:LogoutCommand) => Promise<void> {
+  return async function(command:LogoutCommand): Promise<void> {
+    // TODO: remove session or token associated with user
+    
+  }
 }
