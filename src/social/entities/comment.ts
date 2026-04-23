71c1a65a4b84df3256d9c33d795300845834c759
Taush Sampley <taushsampley@gmail.com>
2026-04-23 18:54:34 -0500

    Add essential fields to Comment entity

--- src/social/entities/comment.ts
+++ src/social/entities/comment.ts
@@ -4,10 +4,14 @@ import type { UUID } from "node:crypto"
  * A single user comment in a feed.
  */
 export class Comment {
+  id:UUID
   text:string
-  userId:UUID
-  constructor(text:string,uuid:UUID) {
+  authorId:UUID
+  parentId:UUID|undefined
+  constructor(id:UUID,text:string,authorId:UUID,parentId?:UUID) {
+    this.id = id
     this.text = text
-    this.userId = uuid
+    this.authorId = authorId
+    this.parentId = parentId
   }
 }
